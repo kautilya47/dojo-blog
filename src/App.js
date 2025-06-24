605a6fd825d6aeef4df8f03405938b7b2bc6bf55
Kautilya Bhat <kautilya.gtr@gmail.com>
2025-06-24 20:22:15 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import Navbar from './Navbar';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Create from './Create';
+import './App.css'
+import BlogDetails from './BlogDetails';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<Home />}/>
+            <Route exact path="/create" element={<Create />}/>
+            <Route exact path="/blogs/:id" element={<BlogDetails />}/>
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
